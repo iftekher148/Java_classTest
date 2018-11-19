a28d012a354877486e20465327eb2d4d58c26043
iftekher <rocktimcseru50@gmail.com>
2018-11-19 23:27:32 +0600

    class Test problem

---
+++ src/main/sound.java
@@ -0,0 +1,14 @@
+package main;
+
+public class sound {
+	
+	static final double FEET_PER_SECOUND = 1100;
+	static final double TIME = 7.2;
+	public static void main(String[] args) {
+		double result;
+		result=FEET_PER_SECOUND*TIME;
+		System.out.println(result);
+
+	}
+
+}
